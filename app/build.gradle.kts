6070750f244ed5d91ae49109b5f7b4c5f5a6d7ca
manisankarsms <manisankarsms@gmail.com>
2025-01-16 09:38:56 +0530

    teeth selection done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.techbros.myproject"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.0.2"
+        versionCode = 2
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/techbros/myproject/PatientDetailsActivity.kt
+++ app/src/main/java/com/techbros/myproject/PatientDetailsActivity.kt
@@ -1,24 +1,29 @@
 package com.techbros.myproject
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebViewClient
 import android.widget.ArrayAdapter
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.techbros.myproject.databinding.ActivityPatientDetailsBinding
 import com.techbros.myproject.viewModel.PatientDetailsViewModel
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 class PatientDetailsActivity : AppCompatActivity() {
     private val patientDetailsViewModel: PatientDetailsViewModel by viewModels()
+    lateinit var binding: ActivityPatientDetailsBinding
     override fun onCreate(
         savedInstanceState: Bundle?
     ) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityPatientDetailsBinding = DataBindingUtil.setContentView(
+        binding = DataBindingUtil.setContentView(
             this,
             R.layout.activity_patient_details
         )
@@ -37,7 +42,46 @@ class PatientDetailsActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, resources.getStringArray(R.array.gender_array))
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            resources.getStringArray(R.array.gender_array)
+        )
         binding.filledExposedDropdown.setAdapter(adapter)
+
+        // Enable JavaScript (if required by your HTML)
+        binding.webView.settings.javaScriptEnabled = true
+
+        // Set a WebViewClient to handle loading within the app
+        binding.webView.webViewClient = WebViewClient()
+        // Bind the interface
+        binding.webView.addJavascriptInterface(WebAppInterface(this, binding), "AndroidInterface")
+
+        // Load the HTML file from raw folder
+        val htmlContent = readHtmlFileFromRaw(R.raw.tooth)
+        binding.webView.loadDataWithBaseURL("", htmlContent, "text/html", "UTF-8", null)
+    }
+
+    // Function to read the HTML file as a string
+    private fun readHtmlFileFromRaw(resourceId: Int): String {
+        val inputStream = resources.openRawResource(resourceId)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        val stringBuilder = StringBuilder()
+        var line: String? = reader.readLine()
+        while (line != null) {
+            stringBuilder.append(line).append("\n")
+            line = reader.readLine()
+        }
+        reader.close()
+        return stringBuilder.toString()
+    }
+
+    class WebAppInterface(private val context: Context, private val binding: ActivityPatientDetailsBinding) {
+
+        @JavascriptInterface
+        fun onElementsSelected(dataKeys: String) {
+            // Display a toast with the comma-separated string of selected elements
+            binding.etOtherDetails.setText(dataKeys)
+        }
     }
 }
